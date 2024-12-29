74b0847c17da746064f140128be31cf5d35f6ef6
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-12-29 15:40:54 +0100

    fix: handle child process exit status and cleanup in pipex functionality

--- src/processes.c
+++ src/processes.c
@@ -6,7 +6,7 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 11:27:27 by sfarren           #+#    #+#             */
-/*   Updated: 2024/12/29 15:10:28 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/12/29 15:40:32 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,11 @@ void	first_child_handler(int *pipefd, int fd, char **argv, char **envp)
 	}
 	close(pipefd[1]);
 	waitpid(pid, &status, 0);
-	if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_FAILURE)
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 	{
 		close(pipefd[0]);
 		close(fd);
-		exit(EXIT_FAILURE);
+		exit(WEXITSTATUS(status));
 	}
 }
 
@@ -42,27 +42,26 @@ void	second_child_handler(int *pipefd, char **argv, char **envp)
 {
 	int		fd;
 	pid_t	pid;
+	int		status;
 
 	pid = fork_child();
 	if (pid == 0)
 	{
-		if (dup2(pipefd[0], STDIN_FILENO) == -1)
-		{
-			perror("dup2 pipe read end");
-			exit(EXIT_FAILURE);
-		}
+		dup2_wrapper(pipefd[0], STDIN_FILENO);
 		fd = open_file(argv[4], O_CREAT | O_WRONLY | O_TRUNC);
-		if (dup2(fd, STDOUT_FILENO) == -1)
-		{
-			perror("dup2 output file");
-			exit(EXIT_FAILURE);
-		}
-		close(pipefd[0]);
-		close(pipefd[1]);
+		dup2_wrapper(fd, STDOUT_FILENO);
+		close_fds(pipefd, 2);
 		close(fd);
 		execute_command(argv[3], envp);
 		perror("execute_command");
 		exit(EXIT_FAILURE);
+	}
+	close(pipefd[0]);
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+	{
+		close(pipefd[1]);
+		exit(WEXITSTATUS(status));
 	}
 }
 
