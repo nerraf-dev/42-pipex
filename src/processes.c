08e593643e925a725758731e45dc689446fea387
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2025-01-01 19:16:15 +0100

    fix: improve error handling for file opening in second_child_handler

--- src/processes.c
+++ src/processes.c
@@ -6,7 +6,7 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 11:27:27 by sfarren           #+#    #+#             */
-/*   Updated: 2025/01/01 18:58:02 by sfarren          ###   ########.fr       */
+/*   Updated: 2025/01/01 19:15:36 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,15 +47,17 @@ void	second_child_handler(int *pipefd, char **argv, char **envp, int cmd_status)
 	if (cmd_status != 0)
 	{
 		close(pipefd[0]);
+		ft_printf_fd(STDERR_FILENO, "pipex: %s: open: No such file or directory\n", argv[4]);
 		return;
 	}
-
 
 	pid = fork_child();
 	if (pid == 0)
 	{
 		dup2_wrapper(pipefd[0], STDIN_FILENO);
 		fd = open_file(argv[4], O_CREAT | O_WRONLY | O_TRUNC);
+		if (fd == -1)
+			exit(EXIT_FAILURE);
 		dup2_wrapper(fd, STDOUT_FILENO);
 		close_fds(pipefd, 2);
 		close(fd);
